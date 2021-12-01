389f9b0a7a64468cb6835803978ddf8bf929aacb
양강현 <93499076+queenjerry@users.noreply.github.com>
2021-12-01 22:26:04 +0900

    Add files via upload

---
+++ 003.js
@@ -0,0 +1,22 @@
+// 변수의 타입
+// 다음 출력 값으로 올바른 것은?
+
+let arr = [100, 200, 300];
+console.log(typeof(arr));
+
+//1) undefined
+//2) String
+//3) Number
+//4) Object
+
+// 4번. 1,2,3번 모두 기본 자료형.
+
+
+
+
+
+//자바스크립트 변수의 타입
+
+// 원시 타입 : number, string, boolean, undefined, null
+
+// 참조 타입 : object
